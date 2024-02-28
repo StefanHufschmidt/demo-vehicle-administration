bbcda15967e20a8bb1190cae3b1f1696368ac70e
Stefan Decker <github@deckermail.info>
2024-02-28 11:24:50 +0100

    Adds test for input validation of login payload

---
+++ backend/src/test/kotlin/info/deckermail/vehicle_administration/backend/login/LoginControllerSpec.kt
@@ -0,0 +1,106 @@
+package info.deckermail.vehicle_administration.backend.login
+
+import org.intellij.lang.annotations.Language
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.post
+
+@WebMvcTest
+class LoginControllerSpec {
+
+    companion object {
+        @JvmStatic
+        @Language("JSON")
+        fun provideInvalidPayload() = listOf(
+            Arguments.of(
+                """
+                {
+                  "username": "",
+                  "password": "hello"
+                }
+                """.trimIndent()
+            ),
+            Arguments.of(
+                """
+                {
+                  "username": "hello",
+                  "password": ""
+                }
+                """.trimIndent()
+            ),
+            Arguments.of(
+                """
+                {
+                  "username": "",
+                  "password": ""
+                }
+                """.trimIndent()
+            ),
+            Arguments.of(
+                """
+                {
+                  "username": "hello"
+                }
+                """.trimIndent()
+            ),
+            Arguments.of(
+                """
+                {
+                  "password": "hello"
+                }
+                """.trimIndent()
+            ),
+            Arguments.of(
+                """
+                {
+                }
+                """.trimIndent()
+            ),
+        )
+    }
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @ParameterizedTest
+    @MethodSource("provideInvalidPayload")
+    internal fun `should reject invalid payload`(invalidPayloadString: String) {
+        // expect: bad request when trying to login with invalid payload
+        mockMvc.post("/login") {
+            content = invalidPayloadString
+            contentType = MediaType.APPLICATION_JSON
+        }.andExpect {
+            status {
+                isBadRequest()
+            }
+        }
+    }
+
+    @Test
+    internal fun `should be able to login with correct payload`() {
+        // given: a correct payload
+        @Language("JSON")
+        val payload = """
+            {
+              "username": "foo",
+              "password": "bar"
+            }
+        """.trimIndent()
+
+        // expect: successful login
+        mockMvc.post("/login") {
+            content = payload
+            contentType = MediaType.APPLICATION_JSON
+        }.andExpect {
+            status {
+                isOk()
+            }
+        }
+    }
+}
